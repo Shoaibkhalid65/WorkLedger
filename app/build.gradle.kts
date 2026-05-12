469f6a8db30269e69ff8d68b74956e2c2873ac9f
Shoaibkhalid65 <gshoaib998@gmail.com>
2026-05-12 15:36:41 +0500

    update to v1.0.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.gshoaib998.progressly"
         minSdk = 26
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.0"
+        versionCode = 4
+        versionName = "1.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
